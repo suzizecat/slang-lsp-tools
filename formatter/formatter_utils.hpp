bb70381dda4d421f51c9648e5599d2826bbbe55a
Julien FAUCHER <suzizecat@free.fr>
2024-03-07 13:04:32 +0100

    Fix compilation issue

--- formatter/formatter_utils.hpp
+++ formatter/formatter_utils.hpp
@@ -4,14 +4,14 @@
 #include <numeric>
 #include <memory>
 
-constexpr size_t sum_dim_vector(const std::vector<size_t>& vec, const unsigned int start = 0)
+inline size_t sum_dim_vector(const std::vector<size_t>& vec, const unsigned int start = 0)
 {
         return std::reduce(
         vec.cbegin() + start, 
         vec.cend(),
         0);
 }
-constexpr size_t sum_dim_vector(const std::vector<std::pair<size_t,size_t>>& vec, const unsigned int start = 0)
+inline size_t sum_dim_vector(const std::vector<std::pair<size_t,size_t>>& vec, const unsigned int start = 0)
 {
     return std::transform_reduce(
         vec.cbegin() + start, 
